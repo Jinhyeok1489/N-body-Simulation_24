613f013f9a61e4f859874286bf31c5d42ff0fd70
Jinhyeok1489 <songjinhuk@dgist.ac.kr>
2017-11-20 23:15:11 +0900

    UML was newly edited
    - UML was edited according to UML.
    - Code was changed slightly.
    > print function seems it doesn't need arguments

--- N_body_simul/Control.h
+++ N_body_simul/Control.h
@@ -12,29 +12,29 @@ private:
 	Set memory; //variable 'memory' saves information of set, particle, force
 public:
 	/*These functions will receive constructors of class force, particle, set*/
-	void ap(double mass_, double x_position, double y_position, double x_velocity = 0, double y_velocity = 0);
+	void ap(double mass_, double x_pos, double y_pos, double x_vel = 0, double y_vel = 0);
 	//add a particle
 	void as(double* set); //add a set
 	void ae(double* set, double* particle); //add a particle to a set
 	void af(double force, double* set, double x_force, double y_force); //add a force to a set
-	//set에 속하지 않은 particle들도 (예를 들어) null 이라는 이름의 set에 다 때려박으면 되겠지 싶다.
 
-	void pm(Set memory);
+
+	void pm();
 	/**
 	 * print out memory usage(number of particles,sets, forces
-	 * @param memory	class array saves information of set, particle, force
+	 * @param			nothing(void) because this function belongs to class Control
 	 * @return			nothing(void)
 	 */
-	void pa(Set memory);
+	void pa();
 	/**
 	* print out all information of memory(particles, sets, forces)
-	* @param			memory class arrays saves information of set, particle, force
+	* @param			nothing(void) because this function belongs to class Control
 	* @return			nothing(void)
 	*/
-	void da(Set memory);
+	void da();
 	/**
 	* delete all particles, sets and forces (memory)
-	* @param			memory
+	* @param			nothing(void) because this function belongs to class Control
 	* @return			nothing(void)
 	*/
 	void ru(double duration);
--- N_body_simul/set.h
+++ N_body_simul/set.h
@@ -8,15 +8,15 @@ using namespace std;
 class Set//make a set of particles
 {
 private:
-	Particle objParticle[];//particle instance
+	Particle *objParticle{};//particle instance array
 	Force objForce[];//force instance
 					 //string setName;// name of a set
 
 public:
 	Set();//constructor
-	void ps();//print informaiton of the set
+	void ps(Set set);//print informaiton of the set
 	//void as();//make a set
 	void ae(Particle objParticle_);//add a particle to the set
 	void de(Particle objParticle_);//delete a particle from the set
-	//void af(Force objForce_, double xForce, double yForce);//add a force to the set whose size is given as a vector (xForce, yForce)
+	void af(Force objForce_, double xForce, double yForce);//add a force to the set whose size is given as a vector (xForce, yForce)
 };
